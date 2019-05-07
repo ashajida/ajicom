fe68726a215bdec7e8a904806322b5f72bad89c2
ASHRAF K AJIDA <ashraf.ajida@yahoo.com>
2019-05-08 00:44:54 +0100

    init

---
+++ assets/script/main.js
@@ -0,0 +1,62 @@
+let burgerBtn = document.querySelector('.burger-menu');
+let menu = document.querySelector('.nav-links');
+
+burgerBtn.addEventListener('click', function () {
+    menu.classList.toggle('open');
+})
+
+
+// Add smooth scrolling to all links
+$("header a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        const hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    } // End if
+});
+
+$(".services a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        const hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    } // End if
+});
+
+$("#about-us a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        const hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    } // End if
+});

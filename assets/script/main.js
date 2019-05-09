2645ae38b643e1f3b1b7ddade7e6f5b1e4c5fc27
ASHRAF K AJIDA <ashraf.ajida@yahoo.com>
2019-05-09 09:38:44 +0100

    modal

--- assets/script/main.js
+++ assets/script/main.js
@@ -98,24 +98,29 @@ function modalOpen(e) {
     modal.classList.add('modal-active');
 
     const value = e.target.attributes.data.value;
-    const mainText = document.querySelector('.modal .main-text p');
+    const modalText = document.querySelector('.modal-text');
+    const title = document.querySelector('.modal .main-text .title');
 
 
     if (value === 'education') {
-        mainText.innerText = `
-            Education accessibility remains a challenge in Africa particularly Zimbabwe where over 60% of the population is domiciled in the remote rural areas and education infrastructure is limited.
-
-            This will ride on the telecommunications above alluded to above. E-Learning is learning utilizing electronic technologies to access educational curriculum outside of a traditional classroom. E learning has gone beyond expectations that advanced courses, programs, degrees and even examinations are being delivered online. In most cases, it refers to a course, program or degree delivered completely online. AJIGOM WIRELESS VSAT Network allows rapid low cost but reliable network reconfiguration and expansion that has the artificial intelligence to meet new and any future unexpected business requirements. Yet its cost effective transmission and network operations are made possible by use of the satellite frequency times division multiple access techniques. Distance learning using AJIGOM WIRELESS VSAT is an innovation that allows educational content to be transmitted from area to area providing students in remote areas to get education of national, regional or international level without limitations of distance or geographical boundaries hence the need of VSAT high speed in order to receive maximum quality real time audio or video content with efficiency. AJIGOM WIRELESS is going to be providing the platform and communication data network used to deliver online via the internet ranging from Distance Education to computerize electronic learning online learning internet learning and many others. 
+        modalText.innerText = `Education accessibility remains a challenge in Africa particularly Zimbabwe where over 60% of the population is domiciled in the remote rural areas and education infrastructure is limited.
+        This will ride on the telecommunications above alluded to above.
+        E-Learning is learning utilizing electronic technologies to access educational curriculum outside of a traditional classroom. E learning has gone beyond expectations that advanced courses, programs, degrees and even examinations are being delivered online. In most cases, it refers to a course, program or degree delivered completely online. AJIGOM WIRELESS VSAT Network allows rapid low cost but reliable network reconfiguration and expansion that has the artificial intelligence to meet new and any future unexpected business requirements. Yet its cost effective transmission and network operations are made possible by use of the satellite frequency times division multiple access techniques. Distance learning using AJIGOM WIRELESS VSAT is an innovation that allows educational content to be transmitted from area to area providing students in remote areas to get education of national, regional or international level without limitations of distance or geographical boundaries hence the need of VSAT high speed in order to receive maximum quality real time audio or video content with efficiency. 
+        AJIGOM WIRELESS is going to be providing the platform and communication data network used to deliver online via the internet ranging from Distance Education to computerize electronic learning online learning internet learning and many others.
         `;
+        title.innerText = 'AJICOM Elearning Wireless Project';
+
     }
 
     if (value === 'technology') {
-        mainText.innerText = `
+        modalText.innerText = `
         Zimbabwe has three mobile network operators being ECONET Zimbabwe which controls +80% of the market and Telecel and Netone which are government owned and have been targeted for privatization in the next 2 years.
 
         AJIGOM WIRELESS is a virtual network operator (VNO) or mobile virtual network operator (MVNO) provider as well as managing the services and a reseller of network services from other telecommunications suppliers that does not own the telecommunication infrastructure. Our bandwidth comes from the most advanced earth station telecom providers that is linked in orbit. AJIGOM WIRELESS VSAT very small aperture terminal is a two way satellite ground station with a dish antenna that is located at the company premises in Harare, Zimbabwe. The subsidiary is managed by qualified staff in the areas of communication at international level both in retail and commercial applications. AJIGOM WIRELESS VSAT bandwidth is leased from large telecom providers with access to satellites in geosynchronous orbit or geostationary orbit to relay data from small remote earth stations to other terminals or master earth station “hubs”. AJIGOM WIRELESS VSAT will also be able to transmit narrowband data
          for POS machines transacting when purchasing using bank cards.
         `;
+
+        title.innerText = 'Telecommunication Project';
     }
 };
 
